c7b0eaeed544bde503e0b76685a3e88ccd7cb013
amey247 <82931448+amey247@users.noreply.github.com>
2023-02-14 00:03:46 -0800

    finished string server!

--- StringServer.java
+++ StringServer.java
@@ -2,25 +2,23 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
+    // The one bit of state on the server: a string that will be manipulated by
     // various requests.
-    int num = 0;
+    String text = "";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Amey's Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
+            return text;
+        } else if (url.getPath().contains("add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                text += parameters[1];
+                text += "\n";
             }
+            return text;
+        } else if (url.getQuery().contains(" ")) {
+            return text;
+        } else {
             return "404 Not Found!";
         }
     }
